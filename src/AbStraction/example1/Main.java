d6fa526a0c5b8020808e484b12a1df66e6620465
vikas <vikasltce@gmail.com>
2025-04-03 12:17:15 +0530

    TH

--- src/AbStraction/example1/Main.java
+++ src/AbStraction/example1/Main.java
@@ -13,3 +13,22 @@ public class Main {
         dg.gender();
     }
 }
+/*
+An abstract class can have both abstract and non-abstract methods.
+The non-abstract methods (methods with a body) can have implementation.
+ From Java 8 onwards, abstract classes can also have default and static methods.
+ An abstract class can implement an interface.
+  This means an abstract class can provide some or all of the implementation required by an interface.
+  Feature	Interface	Abstract Class
+Methods	Only abstract methods (until Java 8); from Java 8 onwards, can have default and static methods.	Can have both abstract and non-abstract methods (with implementation).
+Final Variables	All variables are implicitly public, static, and final.	Can have final and non-final variables.
+Type of Variables	All variables are public, static, and final.	Can have public, protected, private, static, and non-static variables.
+Implementation	Cannot provide implementation of methods.	Can provide partial implementation (abstract methods with or without implementation).
+Inheritance	Interface can be implemented using implements keyword.	Abstract class can be extended using extends keyword.
+Multiple Inheritance	Can implement multiple interfaces.	Can extend only one abstract class but can implement multiple interfaces.
+Access Control	All members are public by default.	Members can have different access modifiers (private, protected, public).
+Conclusion:
+Use an interface when you want to define a contract or behavior that multiple classes can implement, particularly when you expect the classes to behave in a similar way but don't need shared implementation.
+
+Use an abstract class when you want to share code among multiple related classes while still enforcing some common behavior in subclasses.
+ */
--- src/Interface/ex1/Main.java
+++ src/Interface/ex1/Main.java
@@ -11,4 +11,25 @@ public class Main {
 //        Brake b1 = new Car();
 //        b1.start(); cant access child class as car is child of brake, engine as it implements it  but can
     }
+
 }
+/*
+ By default, an interface can only have abstract methods (methods without a body). However, starting from Java 8,
+  interfaces can also have default methods (methods with a body) and static methods.
+  But the default methods are still abstract if they're not defined in the interface.
+
+  interface Animal {
+    void sound();  // abstract method (no body)
+
+    // Default method (Java 8 and onwards)
+    default void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+
+    // Static method
+    static void breathe() {
+        System.out.println("Animal is breathing");
+    }
+}
+
+ */
